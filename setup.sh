2cc73c3ed0aef78721a619c2f38704c8ee242df2
Citren0 <cameron.davis1944@gmail.com>
2024-11-03 15:38:57 -0700

    Changed nginx config.

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@ apt install nginx
 
 openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out cert.crt -keyout key.key
 
-mkdir /etc/certs
+mkdir -p /etc/certs
 
 mv cert.crt /etc/certs/
 mv key.key /etc/certs/
 
-cp nginx.conf /etc/nginx/conf.d/
+cp raspberry-cam /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/raspberry-cam /etc/nginx/sites-enabled/
