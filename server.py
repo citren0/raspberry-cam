a3fa23dd5b0f10fcd1762896ed0bc2221169be4c
Citren0 <cameron.davis1944@gmail.com>
2024-11-02 23:24:07 -0600

    Reverted code structure.

--- server.py
+++ server.py
@@ -3,24 +3,21 @@ import cv2
 from flask_socketio import SocketIO, emit
 import base64
 import time
-import threading
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
 camera = ""
-lock = ""
 
-def get_frame():
-    if lock != "":
-        with lock:
+
+def generate_frames():
+    while True:
+        if camera != "":
             ret, img = camera.read()
             if ret:
                 _, buffer = cv2.imencode('.jpg', img)
                 frame = base64.b64encode(buffer).decode('utf-8')
-                return f"data:image/jpeg;base64,{frame}\n\n"
-    else:
-        return f"data:image/jpeg;base64,asdf\n\n"
+                yield f"data:image/jpeg;base64,{frame}\n\n"        
 
 
 # Flask routes.
@@ -30,15 +27,16 @@ def index():
 
 
 # SocketIO "routes".
-@socketio.on('get_frame')
-def get_frame_socket():
-    emit('video_frame', {'image': get_frame()}, broadcast=True)
+@socketio.on('video_stream')
+def handle_video():
+    for frame in generate_frames():
+        emit('video_frame', {'image': frame}, broadcast=True)
+        time.sleep(0.05)
 
 
 
 
 def main():
-    lock = threading.Lock()
     camera = cv2.VideoCapture(0)
     socketio.run(app, debug=True, port=5000, host="192.168.0.140", use_reloader=False)
 
