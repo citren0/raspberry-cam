b3d1d8de689e4ea6817c2e29336d66c1e172444a
Citren0 <cameron.davis1944@gmail.com>
2024-11-02 22:35:45 -0600

    Working with audio.

--- server.py
+++ server.py
@@ -8,18 +8,60 @@ import pyaudio
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-def generate_audio():
-    p = pyaudio.PyAudio()
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
 
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=44100,
-                    output=True,
-                    stream_callback=None)
+ 
+audio1 = pyaudio.PyAudio()
+ 
 
-    while True:
-        data = stream.read(1024)
-        yield data
+
+def genHeader(sampleRate, bitsPerSample, channels):
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
+
+@app.route('/audio')
+def audio():
+    # start Recording
+    def sound():
+
+        CHUNK = 1024
+        sampleRate = 44100
+        bitsPerSample = 16
+        channels = 2
+        wav_header = genHeader(sampleRate, bitsPerSample, channels)
+
+        stream = audio1.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,input_device_index=1,
+                        frames_per_buffer=CHUNK)
+        print("recording...")
+        #frames = []
+        first_run = True
+        while True:
+           if first_run:
+               data = wav_header + stream.read(CHUNK)
+               first_run = False
+           else:
+               data = stream.read(CHUNK)
+           yield(data)
+
+    return Response(sound())
 
 
 def generate_frames():
@@ -37,23 +79,13 @@ def generate_frames():
 def index():
     return render_template('index.html')
 
-@app.route('/audio')
-def audio():
-    return Response(generate_audio(), 
-                    mimetype='audio/x-wav')
 
-
+# SocketIO "routes".
 @socketio.on('video_stream')
 def handle_video():
     for frame in generate_frames():
         emit('video_frame', {'image': frame}, broadcast=True)
         time.sleep(0.05)
-
-@socketio.on('audio_stream')
-def handle_audio():
-    for audio in generate_audio():
-        emit('audio_data', {'audio': audio}, broadcast=True)
-        time.sleep(0.01)
 
 
 def main():
