cf3a20a7ef1615e848df610f56375f52e209d880
Citren0 <cameron.davis1944@gmail.com>
2024-11-02 23:16:51 -0600

    changing code structure.

--- server.py
+++ server.py
@@ -3,19 +3,22 @@ import cv2
 from flask_socketio import SocketIO, emit
 import base64
 import time
+import threading
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+camera = ""
+lock = ""
 
-def generate_frames():
-    camera = cv2.VideoCapture(2)
-    while True:
-        ret, img = camera.read()
-        if ret:
-            _, buffer = cv2.imencode('.jpg', img)
-            frame = base64.b64encode(buffer).decode('utf-8')
-            yield f"data:image/jpeg;base64,{frame}\n\n"
+def get_frame():
+    if lock != "":
+        with lock:
+            ret, img = camera.read()
+            if ret:
+                _, buffer = cv2.imencode('.jpg', img)
+                frame = base64.b64encode(buffer).decode('utf-8')
+                return f"data:image/jpeg;base64,{frame}\n\n"
 
 
 # Flask routes.
@@ -25,14 +28,16 @@ def index():
 
 
 # SocketIO "routes".
-@socketio.on('video_stream')
-def handle_video():
-    for frame in generate_frames():
-        emit('video_frame', {'image': frame}, broadcast=True)
-        time.sleep(0.05)
+@socketio.on('get_frame')
+def get_frame_socket():
+    emit('video_frame', {'image': get_frame()}, broadcast=True)
+
+
 
 
 def main():
+    lock = threading.Lock()
+    camera = cv2.VideoCapture(2)
     socketio.run(app, debug=True, port=5000, host="192.168.0.140")
 
 
